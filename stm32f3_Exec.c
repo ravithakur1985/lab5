33addb2b18601bff31ba18d545fcced51fb4033b
ravithakur1985 <ravichawhan.thakur@gmail.com>
2019-02-09 20:25:03 -0800

    Updating the Executive code and header files with those sent by the instructor

--- stm32f3_Exec.c
+++ stm32f3_Exec.c
@@ -1,50 +1,97 @@
-///////////////////////////////////////
-// stm32f3_EXEC code                 //
-// Author: Ravi Thakur               //
-// Version 1.0                       //
-//                                   //
-///////////////////////////////////////
-
+#include "stm32f30x.h"
 #include "stm32f3_Exec.h"
-#include "stm32f3_LEDS.h"
-#include "stm32f3_Button.h"
-#include "stm32f3_GeneralTImer2.h"
 
-//defining the file scope variable
-static uint32_t tasks_to_execute;
+static uint32_t ExecTaskFlag;
+static ExecFunctionPointer ExecTaskList[MAX_TASK_COUNT];
 
-//defining the functions
-void Exec_Init(void) {
-	tasks_to_execute = EXEC_TASK_NOP;
-	return;
+//
+// This function will initialize the table of function pointers for the executive
+//
+void Exec_Init(void){
+    uint32_t i;
+
+    //
+    // No tasks need to run yet
+    //
+    ExecTaskFlag = 0;
+    
+    //
+    // Set all tasks to NULL, no task handler present
+    //
+    for (i=0; i<MAX_TASK_COUNT; i++){
+        ExecTaskList[i] = NULL;
+    }
+    
+    //
+    // Add the actual tasks you want to run
+    //
+    ExecTaskList[0] = Button_Task;
+    ExecTaskList[1] = TIM2_Task;
+    
+    return;
 }
 
+//
+// The executive, we never return from here.  We stay in our loop waiting for a task to be set to
+// run.
+//
+void Exec_Start(void){
+    uint32_t i;
+    
+    //
+    // Do this forever!
+    //
+    while(1){    
+
+	//
+	// If ExecTaskFlag is not 0, some task needs to run
+	//
+        if (ExecTaskFlag){        
+	    //
+	    // Go through each task and check which bit is set in ExecTaskFlag
+	    // 
+            for(i=0; i< MAX_TASK_COUNT; i++){
+		//
+		// If this bit is set, clear the task (in case the task needs to set itself
+		// up to run again) and if there is a handler go ahead and call it.
+		//
+                if (ExecTaskFlag & (0x1 << i)){
+                    Exec_ClearTask(0x1 << i);
+                    if (ExecTaskList[i]){    //This protects against calling a NULL handler!  That would crash us!
+                        (ExecTaskList[i])(); //Call the Task we want to execute  
+                    }
+                }
+            }                
+        }else{
+            //
+            // No tasks are needed, go to sleep -- DO NOT wfi, causes problems with debugger!
+            //           
+            __nop();
+        }
+    }
+}
+
+//
+// Set the given task to run
+//
 void Exec_SetTask(uint32_t task){
-	tasks_to_execute = task;	
-	return;
+    
+    //
+    // Set a bit indicating a specific task is needed to run
+    //
+    ExecTaskFlag |= task;
+    return;
 }
 
-void Exec_ClearTask(void){
-	tasks_to_execute = EXEC_TASK_NOP;
-	return;
-}
-
-void Exec_Start(void) {
-	while (1){
-		switch(tasks_to_execute){
-				case 1 :
-					Exec_ClearTask();
-					Button_Task();;
-					break;
-				case 2 :
-          Exec_ClearTask();
-          TIM2_Task();				
-					break;				
-				default:
-					__nop();
-				  break;
-			}
-		
-	}	
+//
+// Clear the given task so it does not run again until set again
+//
+void Exec_ClearTask(uint32_t task){
+    
+    //
+    // Clear a bit indicating a specific task has executed
+    //
+    ExecTaskFlag &= ~task;
+    return;
 }
 
--- stm32f3_Exec.h
+++ stm32f3_Exec.h
@@ -1,27 +1,22 @@
-///////////////////////////////////////
-// stm32f3_Exec header file          //
-// Author: Ravi Thakur               //
-// Version 1.0                       //
-//                                   //
-///////////////////////////////////////
-
-
 #ifndef __stm32f3_EXEC_H__
 #define __stm32f3_EXEC_H__
 #include "stm32f30x.h"
 
-//declaring prototypes
+#define MAX_TASK_COUNT 32
+#define NULL 0
+typedef void (*ExecFunctionPointer)(void);
+
 void Exec_Init(void);
 void Exec_Start(void);
 void Exec_SetTask(uint32_t task);
-void Exec_ClearTask(void);
+void Exec_ClearTask(uint32_t task);
 
-void extern Button_Task(void);
-void extern TIM2_Task(void);
 
-//task macro definitions
-#define EXEC_TASK_NOP    0x00000000
+extern void Button_Task(void);
+extern void TIM2_Task(void);
+
 #define EXEC_TASK_BUTTON 0x00000001
 #define EXEC_TASK_TIM2   0x00000002
 
 #endif
+
